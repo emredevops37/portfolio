6138dc5e5d4a650a9e9a7176b3db78678bfcf577
Emre Aktas <emre.aktas@asiselektronik.com.tr>
2026-08-19 11:59:10 +0300

    3 year yerine 4 years yazildi

--- components/about.tsx
+++ components/about.tsx
@@ -24,7 +24,7 @@ export default function About() {
 
       {language === 'en' ? (
         <p className="mb-3">
-          Hi, I'm Emre, a passionate <span className="font-medium">DevOps Engineer</span> with over <span className="font-medium">3 years of experience</span> in the industry. My primary focus is on
+          Hi, I'm Emre, a passionate <span className="font-medium">DevOps Engineer</span> with over <span className="font-medium">4 years of experience</span> in the industry. My primary focus is on
           automating IT processes and building efficient <span className="font-medium">CI/CD pipelines</span> to streamline development and operations. I have hands-on experience with{' '}
           <span className="font-medium">containerization, orchestration, and infrastructure automation tools</span>, and I specialize in leveraging cloud solutions like{' '}
           <span className="font-medium">AWS, GCP, and Azure</span> to create scalable, reliable systems. My expertise also includes{' '}
